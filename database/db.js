152bbe0825c0a4c6f1ba2c324143c4119594bad2
Inder <inderpreet171222@gmail.com>
2023-03-23 18:17:10 +0530

    Refactor

--- database/db.js
+++ database/db.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose"
-export const connection=async()=>
-{
-    try {
-    await mongoose.connect("mongodb://127.0.0.1:27017", {useunifiedtopology:true})
+import mongoose from "mongoose";
+export const connection = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://NoteBook:W-rU-ZuWMbS3Bc.@cluster0.t7hd0io.mongodb.net/?retryWrites=true&w=majority",
+      { useunifiedtopology: true }
+    );
     console.log("Connected to the database");
-} catch (error) {
+  } catch (error) {
     console.log("Error");
-}
-}
-
-
+  }
+};
